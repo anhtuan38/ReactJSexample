50b4dca0db8f59f6628b7ef4a84c2a41fdf35ea4
AnhTuan <tuanphan204@gmail.com>
2022-03-27 22:53:31 +0700

    todo with useReducer

--- src/App.js
+++ src/App.js
@@ -1,58 +1,93 @@
-import { useState, useMemo, useRef } from "react";
+import { useReducer, useRef } from "react";
+// 1. initial state
+const initState = {
+  job: "",
+  jobs: [],
+};
+
+// 2. Actions
+const SET_JOB = "set_job";
+const ADD_JOB = "add_job";
+const DELETE_JOB = "delete_job";
+
+const setJob = (payload) => {
+  return {
+    type: SET_JOB,
+    payload,
+  };
+};
+const addJob = (payload) => {
+  return {
+    type: ADD_JOB,
+    payload,
+  };
+};
+const deleteJob = (payload) => {
+  return {
+    type: DELETE_JOB,
+    payload,
+  };
+};
+// 3. Reducer
+const reducer = (state, action) => {
+  console.log("prev state", state);
+  console.log("action", action);
+  let newState;
+  switch (action.type) {
+    case SET_JOB:
+      newState = { ...state, job: action.payload };
+      break;
+    case ADD_JOB:
+      newState = {
+        ...state,
+        jobs: [...state.jobs, action.payload],
+      };
+      break;
+    case DELETE_JOB:
+      const newJob = [...state.jobs];
+      newJob.splice(action.payload, 1);
+      newState = {
+        ...state,
+        jobs: newJob,
+      };
+      break;
+    default:
+      throw new Error("Invalid action");
+  }
+  console.log("newState", newState);
+  return newState;
+};
+//4. dispatch
 
 function App() {
-  const [name, setName] = useState("");
-  const [price, setPrice] = useState("");
-  const [products, setProducts] = useState([]);
-
-  const nameRef = useRef();
+  const [state, dispatch] = useReducer(reducer, initState);
+  const { job, jobs } = state;
+  const h1ref = useRef();
 
   const handleSubmit = () => {
-    setProducts([
-      ...products,
-      {
-        name,
-        price: Number(price),
-      },
-    ]);
-    setName("");
-    setPrice("");
-    nameRef.current.focus();
+    dispatch(addJob(job));
+    dispatch(setJob(""));
+    h1ref.current.focus();
   };
 
-  const total = useMemo(() => {
-    const result = products.reduce((total, product) => {
-      console.log("tính toán lại ...");
-      return total + product.price;
-    }, 0);
-    return result;
-  }, [products]);
-
   return (
-    <div style={{ padding: "10px 32px" }}>
+    <div className="App" style={{ padding: 20 }}>
+      <h1>ToDo</h1>
       <input
-        ref={nameRef}
-        value={name}
-        placeholder="Enter name..."
-        onChange={(e) => setName(e.target.value)}
-      />
-      <br />
-      <input
-        value={price}
-        placeholder="Enter price..."
-        onChange={(e) => setPrice(e.target.value)}
-      />
-      <br />
-      <button onClick={handleSubmit}>Add</button>
-      <br />
-      Total: {total}
-      <ul>
-        {products.map((product, index) => (
+        onChange={(e) => dispatch(setJob(e.target.value))}
+        value={job}
+        ref={h1ref}
+        placeholder="add job"
+      ></input>
+      <button onClick={handleSubmit}>add</button>
+      <div style={{ margin: 20 }}>
+        {jobs.map((job, index) => (
           <li key={index}>
-            {product.name} - {product.price}
+            {job}
+            <span onClick={() => dispatch(deleteJob(index))}> &times;</span>
           </li>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
