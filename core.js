866d0dde47b386693cdeca956e2be52eaec8d0b7
AnhTuan <tuanphan204@gmail.com>
2022-04-08 19:48:59 +0700

    library build UI

---
+++ core.js
@@ -0,0 +1,26 @@
+export function createStore(reducer) {
+  let state = reducer();
+  const roots = new Map();
+
+  function render() {
+    for (const [root, component] of roots) {
+      const output = component();
+      root.innerHTML = output;
+    }
+  }
+  return {
+    attach(component, root) {
+      roots.set(root, component);
+      render();
+    },
+    connect(selector = (state) => state) {
+      return (component) =>
+        (props, ...args) =>
+          component(Object.assign({}), props, selector(state), ...args);
+    },
+    dispatch(action, ...args) {
+      state = reducer(state, action, args);
+      render();
+    },
+  };
+}
